ad8dce183bd68b91c0e1847694c4cee03808f21d
tor <torfrancis447@gmail.com>
2023-04-17 22:16:41 -0400

    enable cors, messages channel setup

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,12 @@
 class Message < ApplicationRecord
+    after_create_commit { broadcast_message }
+
+    private
+
+    def broadcast_message
+        ActionCable.sever.braodcast("MessagesChannel", {
+            id:,
+            body:
+        })
+    end
 end
